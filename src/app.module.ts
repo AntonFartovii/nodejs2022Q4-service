46948b6a0a26479356235682a2bcada0248bd202
AntonFartovii <anton@rza.by>
2023-01-31 16:33:35 +0300

    init: nest components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { ArtistsModule } from './artists/artists.module';
+import { TracksController } from './tracks/tracks.controller';
+import { TracksModule } from './tracks/tracks.module';
+import { AlbumsService } from './albums/albums.service';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ArtistsModule, TracksModule, AlbumsModule],
+  controllers: [AppController, UsersController, TracksController],
+  providers: [AppService, UsersService, AlbumsService],
 })
 export class AppModule {}
